afdd637865b5fbf1e753a8be4690add856e4df0c
LJThao <yengfong.thao@atlasschool.com>
2024-03-03 18:49:41 +0000

    printf_c.c

---
+++ printf_c.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * printf_c - prints character for conversion specifiers
+ * @c: char arguments
+ * Return: 1
+ */
+
+int printf_c(va_list c)
+{
+	char str;
+
+	str = va_arg(c, int);
+	_putchar(str);
+	return (1);
+}
