71e0034c6a4edb9020ba996329c6fa65c3661d96
LJThao <yengfong.thao@atlasschool.com>
2024-03-03 20:26:47 +0000

    printf.c

---
+++ printf.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _printf - function that produces output according to a format
+ * @format: the format string
+ * Return: int
+ */
+
+int _printf(const char *format, ...)
+{
+
+
+
+
+
+
+
+
+}
