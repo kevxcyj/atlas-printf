181af9d7a3dbe411bdc4cb403a2f348d935e649d
kevxcyj <kevcyjunior@gmail.com>
2024-03-04 02:11:10 +0000

    added some comments to the main file, also created file for task one C

--- main.h
+++ main.h
@@ -4,11 +4,25 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+int _putchar (char c);
+/* Function for _putchar */
+
 int _printf(const char *format, ...);
+/* Task zero, a function that produces output according to a format */
+
+int printf_char(va_list las)
+/* Task zero, function that prints characters */
+
+
 int printf_c(va_list c);
+
 int printf_i(va_list i);
+/* Task one, function for conversion specifier i */
+
 int printf_s(va_list s);
+
 int _strlen(char *str);
+
 int _strlen_c(const char *str);
 
 
