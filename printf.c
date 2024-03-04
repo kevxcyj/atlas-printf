01d9c3f9bec186acf7b5d8231ce52cb78a4c2a41
kevxcyj <kevcyjunior@gmail.com>
2024-03-04 18:55:13 +0000

    added file

--- printf.c
+++ printf.c
@@ -17,7 +17,7 @@ int _printf(const char *format, ...)
 	int d = 0, e = 0;
 	int f;
 
-	va_start (list, format);
+	va_start(list, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
