7aa66a859afcbcc2b507d6ada0638db648cac071
LJThao <yengfong.thao@atlasschool.com>
2024-03-05 02:28:06 +0000

    deletion of files

--- main.h
+++ main.h
@@ -9,27 +9,6 @@
 int _putchar(char c);
 /* Function for _putchar */
 
-int _printf(const char *format, ...);
-/* Task zero, a function that produces output according to a format */
-
-int printf_c(va_list las);
-/* Task zero, function that prints characters */
-
-int printf_s(va_list las);
-/* Task zero, function that prints S */
-
-int print_perc(void);
-/* Task zero, function that prints % */
-
-int _strlen(char *str);
-/* Task zero, function for printing string */
-
-int _strlen_c(const char *str);
-/* Task zero, function for printing string */
-
-int printf_i(va_list i);
-/* Task one, function for conversion specifier i */
-
 int printf_d(va_list i);
 int binary(int num);
 int octal(int num);
