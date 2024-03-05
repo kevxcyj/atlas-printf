69f5ace7ddbb0924de9acf7f5fbe270318b6b0bd
LJThao <yengfong.thao@atlasschool.com>
2024-03-05 02:39:37 +0000

    debugging

--- main.h
+++ main.h
@@ -6,15 +6,10 @@
 #include <unistd.h>
 #include <string.h>
 
-int _putchar(char c);
-/* Function for _putchar */
-
 int _printf(const char *format, ...);
 /* Task zero, a function that produces output according to a format */
 
-int printf_d(va_list i);
 int binary(int num);
 int octal(int num);
-
 
 #endif
